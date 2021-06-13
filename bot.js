3a39d31903052045961ec1218b0017595129355c
hoosengold <hoosengold@protonmail.com>
2021-06-13 20:51:22 +0200

    Fix crash bug

--- bot.js
+++ bot.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js"),
     //config = require("./config.json"),
     bot = new Discord.Client(),
     prefix = "!",
-    poll = require("../util/poll.js"),
-    setInterval = require("../util/countdownTimer.js");
+    poll = require("./util/poll.js"),
+    setInterval = require("./util/countdownTimer.js");
     //emojiArray = require("../util/emojiArray.js");
 
 require('dotenv').config();
