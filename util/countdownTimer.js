5c1f9228d2b876a50f6b68591d41047f9d1cd5e0
hoosengold <hoosengold@protonmail.com>
2021-06-15 10:02:49 +0200

    Add messaging

--- util/countdownTimer.js
+++ util/countdownTimer.js
@@ -5,50 +5,34 @@ var hoursLeft = require('../bot.js');
 //var timeleft = hoursLeft * 3600000;
 
 // Update the count down every 1 second
-function countdown(message, args){
-    //Parse the amount of hours into variable
-    hoursLeft = parseInt(args[0]);
+function countdown(message, args) {
+  //Parse the amount of hours into variable
+  hoursLeft = parseInt(args[0]);
 
-    //Get the date when the countdown should end
-    var countdownDate = new Date();
-    countdownDate.setUTCHours(countdownDate.getUTCHours() + hoursLeft);
+  //Get the date when the countdown should end
+  var countdownDate = new Date();
+  countdownDate.setUTCHours(countdownDate.getUTCHours() + hoursLeft);
 
-    //Message when the countdow ends
-    message.channel.send('The Countdown will end after ' + hoursLeft + ' hours on **' + countdownDate +'**');
+  //Message when the countdow ends
+  message.channel.send('The Countdown will end after ' + hoursLeft + ' hours on **' + countdownDate + '**');
 
-    setInterval(function() {
-      // Get today's date and time
-      var now = new Date();
-      console.log("now: " + now);
+  setInterval(function () {
+    // Get today's date and time
+    var now = new Date();
+    console.log("now: " + now);
 
-      // Find the distance between now and the count down date
-      var distance = countdownDate - now;
-      console.log("distance: " + distance);
-      
-      //Set hoursLeft variable for messaging every hour
-      var hours = hoursLeft;
-      
-      /*
-      //Message the remaining time every hour
-      while (true){
-            
-        if(hoursLeft = hours){
-          continue;
-        } else if (hoursLeft < hours) {
-          console.error();
-        } else if (hoursLeft > hours){
-          message.channel.send('Only ' + hours + 'hours left!');
-          hoursLeft--;
-        } else if (hours = 0){
-          message.channel.send('@everyone Only 1 hour left!');
-          break;
-        }
-      }
+    // Find the distance between now and the count down date
+    var distance = countdownDate - now;
+    console.log("distance: " + distance);
 
-      // If the count down is finished, write some text
-      if (distance < 0) {
-        return message.channel.send('@everyone Voting time has expired!');
-      }*/
+    //Message everyone at the ;ast 1 hour/30 minutes
+    if (distance < 3600100 && distance > 3599900) {
+      message.channel.send("Only 1 hour left!");
+    } else if (distance < 1800100 && distance > 1799900) {
+      message.channel.send("Only 30 minutes left!");
+    } else if (distance <= 0) {
+      message.channel.send('@everyone The time has expired!'); // If the count down is finished, write some text
+    }
   }, 1000);
 }
 
