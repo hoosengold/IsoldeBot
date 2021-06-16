7e56612ca68a112c22975629d594c8b038913a2b
hoosengold <hoosengold@protonmail.com>
2021-06-16 15:45:06 +0200

    Add function and comments

---
+++ util/music.js
@@ -0,0 +1,11 @@
+//Import all required modules
+const Discord = require("../node_modules/discord.js"),
+    music = require('../bot.js');
+
+//The function of the command
+function music(message, args) {
+    
+}
+
+//Export the module so that it can be used by bot.js
+module.exports = music;
