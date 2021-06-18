8c7086f08ef89d9fb9df34f0db7bf7048cfee638
hoosengold <hoosengold@protonmail.com>
2021-06-18 20:14:48 +0200

    Add proper database support

--- util/music.js
+++ util/music.js
@@ -1,63 +1,77 @@
 //Import all required modules
 const Discord = require("../node_modules/discord.js"),
     //fsLib = require('fs'),
-    { Client } = require('pg');
+    { Pool } = require('pg');
 
-var musicSuggestion = require('../bot.js');
+var musicSuggestion = require('../bot.js')
 
-require('dotenv').config();
+require('dotenv').config()
 
 //login details for the database
-const client = new Client({
+const pool = new Pool({
     user: process.env.user,
     password: process.env.password,
     host: process.env.host,
     port: 5432,
-    database: process.env.database
+    database: process.env.database,
+    ssl: { rejectUnauthorized: false }
+})
+
+
+
+//throw an error if there are any idle clients
+pool.on('error', (err, client) => {
+    console.error('Error on idle client', err)
+    process.exit(-1)
 })
 
 
 //The function of the command
-async function music(message, args) {
-    try {
+function music(message, args) {
+    //async function for the queries
+    ; (async () => {
+
         //connect to the database
-        await client.connect();
-        console.log("Connected successfully.");
+        client = await pool.connect()
+        console.log("Connected successfully.")
 
-        //begin transaction
-        await client.query("BEGIN");
+        try {
+            //begin transaction
+            await client.query("BEGIN");
 
-        //extract video ID from the link
-        musicSuggestion = args[0];
-        const videoID = musicSuggestion.split('v=')[1];
-        console.log(`videoID: ${videoID}`);
+            //extract video ID from the link
+            musicSuggestion = args[0];
+            console.log(`musicSuggestion: ${musicSuggestion}`)
+            const videoID = musicSuggestion.split('v=')[1]
+            console.log(`videoID: ${videoID}`)
 
-        //check if the entry is in the database
-        var getAvail = await client.query("select * from music where link = $1", [getAvail]);
-        console.log(`getResult: ${getAvail}`);
+            //check if the entry is in the database
+            const getAvail = await client.query("select * from music where link = $1", [musicSuggestion])
 
-        if (getAvail == 0) { //if it isn't, add it and its ID, send message
-            await client.query("insert music values ($1, $2)", [getAvail, videoID]);
-            await message.channel.send("The song has been successfully added!");
-            console.log(`Item added successfully.`);
-        } else { //if it is, send message
-            await message.channel.send("The song has already been suggested. Happy listening! :purple_heart:");
+            if (getAvail.rows.length === 0) { //if it isn't, add it and its ID, send message
+                await client.query("insert into music values ($1, $2)", [musicSuggestion, videoID])
+                await message.channel.send("The song has been successfully added!")
+                console.log(`Item added successfully.`)
+            } else { //if it is, send message
+                await message.channel.send("The song has already been suggested. Happy listening! :purple_heart:");
+                console.log(`Item already added.`)
+            }
+
+            //end transaction
+            await client.query("COMMIT");
+
+        } finally {
+            //release the client to the pool
+            await client.release()
+            console.log("Client released succesfully.")
         }
 
-        //end transaction
-        await client.query("COMMIT");
 
-
-    } catch (err) {
-        console.log(`Something went wrong: ${err}`);
-    } finally {
-        //end the connection
-        await client.end();
-        console.log("Disconnected succesfully.");
-    }
-
-
-
+    })().catch(err => { //throw an error and rollback in case of an error
+        client.query("ROLLBACK");
+        console.log(`Rollback`);
+        console.log(err.stack);
+    });
 }
 
 //Export the module so that it can be used by bot.js
