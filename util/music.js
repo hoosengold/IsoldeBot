747a84e230eb7a941b855102ef9b71b3059da9ea
hoosengold <hoosengold@protonmail.com>
2021-06-18 17:23:59 +0200

    Implement database

--- util/music.js
+++ util/music.js
@@ -1,14 +1,63 @@
 //Import all required modules
 const Discord = require("../node_modules/discord.js"),
-    fsLib = require('fs');
+    //fsLib = require('fs'),
+    { Client } = require('pg');
+
 var musicSuggestion = require('../bot.js');
 
-//Initialize hash map to store the data
-let musicMap = new Map();
+require('dotenv').config();
+
+//login details for the database
+const client = new Client({
+    user: process.env.user,
+    password: process.env.password,
+    host: process.env.host,
+    port: 5432,
+    database: process.env.database
+})
+
 
 //The function of the command
-function music(message, args) {
-    
+async function music(message, args) {
+    try {
+        //connect to the database
+        await client.connect();
+        console.log("Connected successfully.");
+
+        //begin transaction
+        await client.query("BEGIN");
+
+        //extract video ID from the link
+        musicSuggestion = args[0];
+        const videoID = musicSuggestion.split('v=')[1];
+        console.log(`videoID: ${videoID}`);
+
+        //check if the entry is in the database
+        var getAvail = await client.query("select * from music where link = $1", [getAvail]);
+        console.log(`getResult: ${getAvail}`);
+
+        if (getAvail == 0) { //if it isn't, add it and its ID, send message
+            await client.query("insert music values ($1, $2)", [getAvail, videoID]);
+            await message.channel.send("The song has been successfully added!");
+            console.log(`Item added successfully.`);
+        } else { //if it is, send message
+            await message.channel.send("The song has already been suggested. Happy listening! :purple_heart:");
+        }
+
+        //end transaction
+        await client.query("COMMIT");
+
+
+    } catch (err) {
+        console.log(`Something went wrong: ${err}`);
+    } finally {
+        //end the connection
+        await client.end();
+        console.log("Disconnected succesfully.");
+    }
+
+
+
 }
 
 //Export the module so that it can be used by bot.js
