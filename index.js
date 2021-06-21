7c56738378113fabef34e4ce94b4ec27f457f10c
hoosengold <hoosengold@protonmail.com>
2021-06-21 23:24:55 +0200

    Rename the client from bot to client

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const Discord = require('./node_modules/discord.js'),
-    bot = new Discord.Client(), //initialize client for the bot;
+    client = new Discord.Client(), //initialize client for the bot;
     prefix = "*", //prefix for all commands
     config = require('./config.json'), //Login with test bot
-    botPrefix = require('./bot.js');
+    bot = require('./bot.js');
 
 
 //Login with deploy bot + 
 //change app id
 //require('dotenv').config();
-//bot.login(process.env.DISCORD_TOKEN);
+//client.login(process.env.DISCORD_TOKEN);
 
 
 //Login with test bot
-bot.login(config.token);
+client.login(config.token);
 
 //Print Ready in the console when the bot is ready
-bot.once("ready", () => {
+client.once("ready", () => {
     console.log(`Ready!`)
 })
 
-bot.on("message", function (message) {
+client.on("message", function (message) {
     if (message.author.bot) return; //checks if the author of the message is a bot, if it is, then it does not respond
     if (!message.content.startsWith(prefix)) return; //checks if the message starts with *, if it does not, then it does not respond
 
@@ -33,7 +33,7 @@ bot.on("message", function (message) {
     console.log(`commandBody: ${commandBody}`)
 
     if (message.content.startsWith(prefix)) {
-        botPrefix(message)
+        bot(message, args)
     }
 
 });
