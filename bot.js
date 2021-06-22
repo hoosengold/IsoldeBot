51f4cdb47653d8420d42190905c869786ff2d7e7
hoosengold <hoosengold@protonmail.com>
2021-06-22 11:28:05 +0200

    Code cleanup

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js'),
     //bot = new Discord.Client(), //initialize client for the bot
     //prefix = "*", //prefix for all commands
     //config = require('./config.json'), //Login with test bot
+    botReq = require('./index.js'),
     poll = require('./util/poll/poll.js'), //import the module required for the poll command
     countdown = require('./util/countdown/countdownTimer.js'), //import the module required for the countdown command
     updates = require('./text/changelog.js'), //import the module required for the updates command
@@ -12,36 +13,8 @@ const Discord = require('discord.js'),
     lore = require('./text/lore.js'), //import the module required for the story command
     typo = require('./text/help.json'); //import the module required for the default switch case
 
-var botReq = require('./index.js');
-
-//Login with deploy bot + 
-//change app id
-//require('dotenv').config();
-//bot.login(process.env.DISCORD_TOKEN);
-
-
-//Login with test bot
-//bot.login(config.token);
-
-//Print Ready in the console when the bot is ready
-/*bot.once("ready", () => {
-    console.log(`Ready!`)
-})*/
 
 function bot(message, args, command){
-
-    /*bot.on("message", function (message) {
-        if (message.author.bot) return; //checks if the author of the message is a bot, if it is, then it does not respond
-        if (!message.content.startsWith(prefix)) return; //checks if the message starts with *, if it does not, then it does not respond
-
-
-        //takes the message body, removes the prefix !, splits the message body and makes everything lower case
-        const commandBody = message.content.slice(prefix.length), //returns everything without the prefix
-            args = commandBody.split(/ +/), //returns args[] where [0] is the first word arfter the command
-            command = args.shift(); //returns the command
-        console.log(`command: ${command}`)
-        console.log(`args: ${args}`)
-        console.log(`commandBody: ${commandBody}`)*/
 
     //check for the different commands
     switch (command) {
@@ -68,7 +41,7 @@ function bot(message, args, command){
             break;
         //default: message.channel.send(typo.default); removed until *word* bug is fixed
     }
-    //});
+   
 }
 
 module.exports = bot;
