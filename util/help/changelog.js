0086cc7d9041f163f3156df7d2c4b410f1f81ca2
hoosengold <hoosengold@protonmail.com>
2021-06-24 22:50:03 +0200

    Restructure for command handler

--- util/help/changelog.js
+++ util/help/changelog.js
@@ -1,40 +1,46 @@
-const Discord = require("discord.js"),
-    changelogMessage = require("../../bot.js");
-//const image = new Discord.MessageAttachment('./images/');
+module.exports = {
+    name: 'updates',
+    description: 'Provide a changelog for the most recent changes.',
+    aliases: ['changes', 'changelog', 'update'],
+    cooldown: 5,
+    args: false,
+    execute(message) {
+        const Discord = require("discord.js");
 
-function updates(message) {
-    const embed = {
-        title: 'The Friend Updates',
-        color: '#D4AB29',
-        description: '\u200b',
-        //thumbnail: 'attachment://',
-        fields: [
-            {
-                name: '**1.** *Everything is prettier now!* :purple_heart: ',
-                value: 'All messages are embeded now and all of them have different colors.'
+        //const image = new Discord.MessageAttachment('./images/');
+
+        const embed = {
+            title: 'The Friend Updates',
+            color: '#D4AB29',
+            description: '\u200b',
+            //thumbnail: 'attachment://',
+            fields: [
+                {
+                    name: '**1.** *Everything is prettier now!* :purple_heart: ',
+                    value: 'All messages are embeded now and all of them have different colors.'
+                },
+                {
+                    name: '\u200b',
+                    value: '\u200b'
+                },
+                {
+                    name: "**2.** *Bugs are no more!* :space_invader:",
+                    value: "Various bug fixes including the infamous \"NOT NOW BOT\" bug (thanks Ultra :grin:)! "
+                },
+                {
+                    name: '\u200b',
+                    value: '\u200b'
+                },
+                {
+                    name: 'BUGS!!! (╯°□°）╯︵ ┻━┻',
+                    value: 'If you encountered a bug or have an idea how to improve The Friend, just @hoosengold and tell him how terrible The Friend is coded. :purple_heart:'
+                },
+            ],
+            footer: {
+                text: 'Last Update:???????',
             },
-            {
-                name: '\u200b',
-                value: '\u200b'
-            },
-            {
-                name: "**2.** *Bugs are no more!* :space_invader:",
-                value: "Various bug fixes including the infamous \"NOT NOW BOT\" bug (thanks Ultra :grin:)! "
-            },
-            {
-                name: '\u200b',
-                value: '\u200b'
-            },
-            {
-                name: 'BUGS!!! (╯°□°）╯︵ ┻━┻',
-                value: 'If you encountered a bug or have an idea how to improve The Friend, just @hoosengold and tell him how terrible The Friend is coded. :purple_heart:'
-            },
-        ],
-        footer: {
-            text: 'Last Update:???????',
-        },
-        timestamp: new Date(),
+            timestamp: new Date(),
+        }
+        message.channel.send({ embed: embed })
     }
-    message.channel.send({ embed: embed })
 }
-module.exports = updates;
