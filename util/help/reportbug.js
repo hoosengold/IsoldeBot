5c5809995e65b35d0ebf0f9d691f3c28ecb5e172
hoosengold <hoosengold@protonmail.com>
2021-06-25 22:59:32 +0200

    Minor changes

--- util/help/reportbug.js
+++ util/help/reportbug.js
@@ -10,7 +10,7 @@ module.exports = {
         const embed = {
             title: 'Report a bug',
             color: 'RANDOM',
-            description: 'If you encountered a bug or have an idea how to improve The Friend, just @hoosengold (or open an [issue](https://github.com/hoosengold/ai_furnee-discord-bot/issues) on Github) and tell him how terrible The Friend is coded.',
+            description: 'If you encountered a bug or have an idea how to improve The Friend, just @hoosengold (or open an [issue](https://github.com/hoosengold/ai_furnee-discord-bot/issues) on Github) and tell him how terrible The Friend is coded. :purple_heart:',
         }
         message.channel.send({ embed: embed })
     }
