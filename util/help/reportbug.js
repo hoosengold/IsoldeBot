0faf0cb5e7f365554b6a58297c154746673ab2f1
hoosengold <hoosengold@protonmail.com>
2021-06-25 21:30:07 +0200

    Add report a bug command

---
+++ util/help/reportbug.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'bug',
+    description: 'Report a bug',
+    aliases: ['bugreport', 'reportbug', 'bugs', 'issue'],
+    cooldown: 5,
+    args: false,
+    execute(message) {
+        const Discord = require('discord.js')
+
+        const embed = {
+            title: 'Report a bug',
+            color: 'RANDOM',
+            description: 'If you encountered a bug or have an idea how to improve The Friend, just @hoosengold (or open an [issue]() on Github)and tell him how terrible The Friend is coded.',
+        }
+        message.channel.send({ embed: embed })
+    }
+}
