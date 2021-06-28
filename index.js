4faac0807e97ed8091a65985faff23f95089af02
hoosengold <hoosengold@protonmail.com>
2021-06-28 15:33:20 +0200

    Add URL Regex

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const help = require('./util/help/help');
-
 const Discord = require('discord.js');
 const client = new Discord.Client({ //initialize client for the bot;
     presence: {
@@ -9,7 +7,7 @@ const client = new Discord.Client({ //initialize client for the bot;
             type: 'PLAYING'
         }
     }
-}); 
+});
 const prefix = "*", //prefix for all commands
     config = require('./config.json'), //Login with test bot
     fs = require('fs');
@@ -75,6 +73,48 @@ client.on("guildMemberAdd", (member) => {
 
 //listen for messages, main function of the bot
 client.on('message', function (message) {
+    const urlRegexMain = new RegExp(/^(?:(?:(?:https|ftp|http|mailto|file|data|irc?):)?\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/gmi);
+    const urlRegexAlphanumeric = new RegExp(/^(?:(?:(?:https|ftp|http|mailto|file|data|irc?):)?\/\/)?([\w\d\-]+\.)+\w{2,}(\/.+)?$/gmi);
+    const urlRegexIPv4 = new RegExp(/^(?:(?:(?:https|ftp|http|mailto|file|data|irc?):)?\/\/)?(((25[0-5])|(2[0-4]\d)|(1\d{2})|(\d{1,2}))\.){3}(((25[0-5])|(2[0-4]\d)|(1\d{2})|(\d{1,2})))$/img);
+    const urlRegexIPv6 = new RegExp(/^(?:(?:(?:https|ftp|http|mailto|file|data|irc?):)?\/\/)?(([\da-fA-F]{0,4}:){1,7}[\da-fA-F]{0,4})$/);
+    
+    const testURL = ['www.google.com', 'http://www.google-com.123.com', 'http://www.google.com',
+        'http://www.google-com.123', 'https://www.google-com.com', 'http://google-com.com', 'http://google.com',
+        'google.com', 'http://www.gfh.', 'http://www.gfh.c', 'http://www.gfh:800000', 'www.google.com ',
+        'http://google', '//cdnblabla.cloudfront.net/css/app.css', "//www.google.com",
+        "//cdnblabla.cloudfront.net/css/app.css", "http://✪df.ws/123",
+        "http://userid:password@example.com:8080", "http://userid:password@example.com:8080/",
+        "http://userid@example.com", "http://userid@example.com/", "http://userid@example.com:8080",
+        "http://userid@example.com:8080/", "http://userid:password@example.com",
+        "http://userid:password@example.com/", "http://142.42.1.1/", "http://142.42.1.1:8080/",
+        "http://➡.ws/䨹", "http://⌘.ws", "http://⌘.ws/", "http://foo.com/blah_(wikipedia)#cite-1",
+        "http://foo.com/blah_(wikipedia)_blah#cite-1", "http://foo.com/unicode_(✪)_in_parens",
+        "http://foo.com/(something)?after=parens", "http://☺.damowmow.com/",
+        "http://code.google.com/events/#&product=browser", "http://j.mp", "ftp://foo.bar/baz",
+        "http://foo.bar/?q=Test%20URL-encoded%20stuff", "http://مثال.إختبار", "http://例子.测试",
+        "http://", "http://.", "http://..", "http://../", "http://?", "http://??", "http://??/",
+        "http://#", "http://##", "http://##/", "http://foo.bar?q=Spaces should be encoded", "//",
+        "//a", "///a", "///", "http:///a", "foo.com", "rdar://1234", "h://test", "http:// shouldfail.com",
+        ":// should fail", "http://foo.bar/foo(bar)baz quux", "ftps://foo.bar/", "http://-error-.invalid/",
+        "http://-a.b.co", "http://a.b-.co", "http://0.0.0.0", "http://10.1.1.0", "http://10.1.1.255",
+        "http://224.1.1.1", "http://1.1.1.1.1", "http://123.123.123", "http://3628126748",
+        "http://.www.foo.bar/", "http://www.foo.bar./", "http://.www.foo.bar./", "http://10.1.1.1",
+        "http://10.1.1.254"]
+
+        var t = 0;
+        var f = 0;
+        testURL.forEach(element => {
+            if(urlRegexMain.test(element)|urlRegexAlphanumeric.test(element)|urlRegexIPv4.test(element)|urlRegexIPv6.test(element)){
+                t++;
+            }else{
+                f++;
+            }
+            console.log(`${element}: ${urlRegexMain.test(element)}`)
+        });
+        console.log(`true: ${t}`)
+        console.log(`false: ${f}`)
+    
+
     //checks if the author of the message is a bot, if it is, then it does not respond
     if (message.author.bot) return;
 
