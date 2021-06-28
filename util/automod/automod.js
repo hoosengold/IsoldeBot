bb09f8d112c6da722857d7951667520430e1d662
hoosengold <hoosengold@protonmail.com>
2021-06-28 14:58:38 +0200

    Add automod/

---
+++ util/automod/automod.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js'),
+    config = require('../../config.json'),
+    index = require('../../index.js');
+
+async function automod(message) {
+
+}
+
+module.exports = automod;
