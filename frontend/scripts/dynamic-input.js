b74346cff5c6dadc06d3166de994beeaa497e297
hoosengold <hoosengold@protonmail.com>
2021-07-11 11:41:01 +0200

    Remove frontend/

--- frontend/scripts/dynamic-input.js
+++
@@ -1,9 +0,0 @@
-//var commandType = document.documentElement.id
-function input() {
-    if (commandType === "poll") {
-        var newDiv = document.createElement('div');
-        newDiv.setAttribute('id', 'div_')
-        newDiv.innerHTML = '<input type="text" id="tb_" name="tb_"/>';
-        document.getElementById('addinput').appendChild(newDiv);
-    }
-}
