0acfbb388b34a3c45e367c8e86defa51631bbd3a
hoosengold <hoosengold@protonmail.com>
2021-07-18 22:38:33 +0200

    Implement quiz

---
+++ util/quiz/quiz.js
@@ -0,0 +1,141 @@
+module.exports = {
+    name: 'quiz', //name of the command
+    description: 'Initialize a quiz', //short description of the command
+    //aliases: [], //aliases for the command
+    cooldown: 5, //cooldown for the command in seconds, the default cooldown is 5 seconds
+    args: true, //does the command have arguments, type false if it doesn't and remove args in execute
+    execute(message, args) {
+        const Discord = require('discord.js');
+        const buttons = require('discord-buttons');
+        const emojiArray = require('../poll/pollutil/emojiArray');
+        const pool = require('../../connections/database')
+
+        const questionRegEx = new RegExp(/((\ )*[a-zA-Z]*(\ )*\d*(\ )*)*\?/gm),
+            optionsRegEx = new RegExp(/(?:((?:(\ ))*[a-zA-Z]*(?:(\ ))*\d*(\ )*))*\!/i);
+
+        //throw an error if there are any idle clients
+        pool.on('error', (err, client) => {
+            console.error('Error on idle client', err)
+            process.exit(-1)
+        })
+
+            ; (async () => {
+
+                const client = await pool.connect()
+                console.log("Connected successfully.")
+
+                try {
+                    for (let index = 0; index < args.length; index++) {
+                        console.log(`${args[index]}: ${optionsRegEx.test(args[index])}`)
+                    }
+
+                    //initialize array for the question and options
+                    let i = 0;
+                    let j = 0;
+                    let quizArray = [];
+
+                    //checks if the array element in args ends with ? or !;
+                    //if the element does not have?/!, add it to the current quizArray element
+                    //if it does have?/!, add it to the current quizArray element and call the j+1 element
+                    for (i; i < args.length; i++) {
+                        if (questionRegEx.test(args[i])) {
+                            if (quizArray[j] == undefined) {
+                                quizArray[j] = args[i]
+                            } else {
+                                quizArray[j] = quizArray[j] + ' ' + args[i]
+                            }
+                            j++
+                        } else if (optionsRegEx.test(args[i])) {
+                            if (quizArray[j] == undefined) {
+                                quizArray[j] = args[i]
+                            } else {
+                                quizArray[j] = quizArray[j] + ' ' + args[i]
+                            }
+                            j++
+                        } else {
+                            if (quizArray[j] == undefined) {
+                                quizArray[j] = args[i]
+                            } else {
+                                quizArray[j] = quizArray[j] + ' ' + args[i]
+                            }
+                        }
+                    }
+                    console.log(`quizArray: ${quizArray}`)
+
+                    //checks if the command has title
+                    if (!questionRegEx.test(quizArray[0])) {
+                        return await message.reply('No quiz title/question specified.')
+                    }
+
+                    //checks if the command has options and if they are more than 6
+                    if (!optionsRegEx.test(quizArray[1])) {
+                        return await message.reply('No quiz options specified.')
+                    } else if (quizArray.length > 6) {
+                        return await message.channel.send(`Max. 5 options!`)
+                    }
+
+                    //creates a string for the options
+                    let quizMessage = ''
+                    for (let k = 1; k < quizArray.length; k++) {
+                        if (quizMessage === '') {
+                            quizMessage = `${emojiArray()[k - 1]} ${quizArray[k]} \n\n `
+                        } else {
+                            quizMessage = `${quizMessage} ${emojiArray()[k - 1]} ${quizArray[k]} \n\n `
+                        }
+                    }
+
+                    let quizOptions = ''
+                    for (let k = 1; k < quizArray.length; k++) {
+                        if (quizOptions === '') {
+                            quizOptions = `${quizArray[k]}  `
+                        } else {
+                            quizOptions = `${quizOptions} ${quizArray[k]}   `
+                        }                
+                    }
+
+                    let btnOptions = ['A', 'B', 'C', 'D', 'E']
+
+                    //let row = new buttons.MessageActionRow()
+                    let row = []
+
+                    for (let m = 1; m < quizArray.length; m++) {
+                        let btn = new buttons.MessageButton()
+                            .setStyle('blurple')
+                            .setID(`option${m}`)
+                            .setLabel(`Option ${btnOptions[m - 1]}`)
+
+                        //btnRow.push(btn)
+                        //row.addComponents(btn[m])
+                        row.push(btn)
+                    }
+
+                    //console.log(`row: ${row}`)
+
+                    //get the row count
+                    var rs = await client.query("select * from quiz")
+                    var quizCounter = rs.rows.length;
+
+                    //embed format and content
+                    const embed = {
+                        color: 'RANDOM',
+                        title: `Question ${quizCounter}: ${quizArray[0]}`,
+                        description: `${quizMessage} \n\n`,
+                    }
+
+                    await client.query('insert into quiz(counter, question, options) values ($1, $2, $3)', [quizCounter, quizArray[0], quizOptions])
+                    await message.channel.send({ embed: embed, buttons: row })
+                    //quizCounter++;
+
+                } finally {
+                    //release the client to the pool
+                    await client.release()
+                    console.log("Client released succesfully.")
+                }
+            })().catch(err => { //throw an error and rollback in case of an error
+                //client.query("ROLLBACK");
+                //console.log(`Rollback`);
+                console.log(err.stack);
+            });
+
+    }
+}
