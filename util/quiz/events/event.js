07ede2bc71ae341305f086b1b7149eb6ba2485ad
hoosengold <hoosengold@protonmail.com>
2021-07-26 15:16:37 +0200

    Minor updates

--- util/quiz/events/event.js
+++ util/quiz/events/event.js
@@ -4,14 +4,9 @@ module.exports = {
     execute(button) {
         const Discord = require('discord.js');
         const disbut = require('discord-buttons');
-        //const { quizCounter } = require('../quiz.js')
         const pool = require('../../../connections/database');
         const crypto = require('crypto');
         require('dotenv').config()
-
-
-        console.log(`button event handler reached, button.id: ${button.id}`)
-        //console.log(`quizCounter: ${quizCounter}`)
 
         let buttonID = button.id
         let questionID = buttonID.split('question')[1]
@@ -35,24 +30,18 @@ module.exports = {
                     var hashedID = hasher.update(userID).digest('hex')
                     userID = ''
 
-                    console.log(`hashedID: ${hashedID}`)
-
-                    //get the row count
-                    var rs = await client.query("select * from music")
-                    var rowCount = rs.rows.length
-
                     //check if the entry is in the database
                     const getAvail = await client.query("select user_id from quiz_users where user_id = $1", [hashedID])
                         .then(async result => {
-                            //print the link value
-                            console.table(result.rows[0])
-
                             if (result.rows.length === 0) { //if it isn't, add it
                                 await client.query("insert into quiz_users values ($1)", [hashedID])
                                 console.log(`ID added successfully.`)
                             } else { //if it is, send message
                                 console.log(`ID already added.`)
                             }
+
+                            //print the link value
+                            console.table(result.rows[0])
                         })
                         .catch(console.error())
 
