0db8437ca2167c41b1606068248ace2d6cea3423
hoosengold <73896404+hoosengold@users.noreply.github.com>
2021-08-09 19:00:53 +0200

    Fix message.delete error

--- util/music/musicGet.js
+++ util/music/musicGet.js
@@ -9,38 +9,45 @@ module.exports = {
         const Discord = require("discord.js"),
             db = require("../../connections/database.js");
 
-        //function of the command getMusic
-        ; (async () => {
-            //connect to the database
-            const client = await pool.connect()
-            console.log(`Connected successfully.`)
+        const pool = db.pool
 
-            try {
+            //function of the command getMusic
+            ; (async () => {
+                //connect to the database
+                const client = await pool.connect()
+                console.log(`Connected successfully.`)
 
-                //get the row count
-                var rs = await db.query("select * from music")
-                var rowCount = rs.rows.length
+                try {
 
-                //take a random number for a random row
-                var randomRow = Math.floor(Math.random() * rowCount + 1);
-                console.log(`randomRow: ${randomRow}`)
+                    //get the row count
+                    var rs = await db.query("select * from music")
+                    var rowCount = rs.rows.length
 
-                //select the song from the random row
-                const songSuggestion = await db.query("select link from music where rownumber = $1", [randomRow])
-                    .then(result => {
-                        //message the link 
-                        message.reply(`A random song was chosen for you! Enjoy suggestion *\u2116 ${randomRow}* by our fellow stream fams! :purple_heart: \n\n ${result.rows[0].link}`)
-                    })
+                    //take a random number for a random row
+                    var randomRow = Math.floor(Math.random() * rowCount + 1);
+                    console.log(`randomRow: ${randomRow}`)
 
-            } finally {
-                console.log(`Music fetched successfully.`)
-            }
+                    //select the song from the random row
+                    const songSuggestion = await db.query("select link from music where rownumber = $1", [randomRow])
+                        .then(result => {
+                            //message the link 
+                            message.reply({ content: `A random song was chosen for you! Enjoy suggestion *\u2116 ${randomRow}* by our fellow stream fams! :purple_heart: \n\n ${result.rows[0].link}`, allowedMentions: { repliedUser: true } })
+                        })
 
-        })().catch(err => {
-            console.log(err.stack)
-        });
+                } finally {
+                    console.log(`Music fetched successfully.`)
+                }
+
+            })().catch(err => {
+                console.log(err.stack)
+            });
         //delete the call message
-        message.delete()
-            .catch(err => console.error(err))
+        setTimeout(() => {
+            message.delete()
+                .catch(err => console.error(err))
+            console.log(`Original message deleted.`)
+        }, 5000);
+
+
     }
 }
