1280dae3288a58c9f8c125375aa8175397c59f6b
hoosengold <73896404+hoosengold@users.noreply.github.com>
2021-11-10 19:13:14 +0100

    Split help message into 2 messages to avoid embed limitations

--- commands/help/help.js
+++ commands/help/help.js
@@ -14,31 +14,45 @@ module.exports = {
 		let helpEmbed = new Discord.MessageEmbed()
 			.setAuthor('hoosengold', 'attachment://git_profile-pic.png', 'https://github.com/hoosengold/IsoldeBot')
 			.setColor('RANDOM')
-			.setTitle('Help Menu')
+			.setTitle('IsoldeBot Help Menu')
 			.setDescription('A comprehensive list with all commands and how to use them.')
+
+		let helpEmbed2 = new Discord.MessageEmbed()
+			.setColor(helpEmbed.color)
 			.setTimestamp(new Date())
 			.setFooter(`Note: undefined = no aliases \n Last updated: ` + fs.statSync('commands/help/help.js').mtime.toUTCString())
 
 		const commandFolders = fs.readdirSync('commands')
 
-		//FIXME fix max amount of emebed fields (2 or more embeds)
+		let flag = true
+		Boolean(flag)
+		let j = 0
+
 		for (const folder of commandFolders) {
 			const commandFiles = fs.readdirSync(`commands/${folder}`).filter((file) => file.endsWith('.js')) //filter the command files
 			for (const file of commandFiles) {
+				j++
 				const command = require(`../../commands/${folder}/${file}`)
-				helpEmbed.addField(
-					`\`\*${command.name}\``,
-					`\*${command.description}\* \n __Aliases:__ ${command.aliases} \n __Permissions:__ **${command.permissions}** \n __Syntax:__ \`${command.syntax}\` \n\n \u200b`
-				)
+				if (!(j % 2 == 0)) {
+					helpEmbed.addField(
+						`\`\*${command.name}\``,
+						`\*${command.description}\* \n __Aliases:__ ${command.aliases} \n __Permissions:__ **${command.permissions}** \n __Syntax:__ \`${command.syntax}\` \n\n \u200b`
+					)
+				} else {
+					helpEmbed2.addField(
+						`\`\*${command.name}\``,
+						`\*${command.description}\* \n __Aliases:__ ${command.aliases} \n __Permissions:__ **${command.permissions}** \n __Syntax:__ \`${command.syntax}\` \n\n \u200b`
+					)
+				}
 			}
 		}
 
-		helpEmbed.addField(
+		helpEmbed2.addField(
 			`For more information about the commands:`,
 			`Please refer to [the official GitHub repository](https://github.com/hoosengold/IsoldeBot/tree/main#commands).`
 		)
 
-		message.channel.send({ embeds: [helpEmbed], files: [profilePic] })
+		message.channel.send({ embeds: [helpEmbed, helpEmbed2], files: [profilePic] })
 		//delete the call message
 		message.delete().catch((err) => console.error(err))
 	},
