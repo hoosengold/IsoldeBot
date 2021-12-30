5163b8dd61e984fef5cb2d33536974dc84207ba7
hoosengold <73896404+hoosengold@users.noreply.github.com>
2021-12-30 11:38:08 +0200

    Implement button and select menu handlers

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,16 +1,67 @@
-const events = require('../commands/quiz/events/event')
+const events = require('../commands/quiz/events/event'),
+    { Interaction } = require('discord.js'),
+    setup = require('../commands/initial/setup'),
+    { Util } = require('../typescript/dist/typescript/src/Util')
 
 module.exports = {
     name: 'interactionCreate',
     once: false,
-    execute(interaction) {
+    /**
+     *
+     * @param {Interaction} interaction
+     */
+    async execute(interaction) {
         if (interaction.isButton()) {
-            events.execute(interaction)
-        } else if (interaction.isCommand()) {
-            return
+            if (interaction.customId.includes('Setup')) {
+                handleSetupButtons(interaction)
+            } else {
+                events.execute(interaction)
+            }
+        } else if (interaction.isSelectMenu()) {
+            handleSetupSelectMenu(interaction)
         } else {
             console.log(`No button/command detected in interaction event, exiting event...`)
             return
         }
     },
+}
+
+/**
+ *
+ * @param {Interaction} interaction
+ */
+async function handleSetupButtons(interaction) {
+    const util = new Util(interaction.member.id, interaction.guildId, interaction.client)
+    if (!util.isOwner()) {
+        return interaction.reply({
+            content: `Only the owner can change the settings!`,
+            ephemeral: true,
+        })
+    }
+
+    const customId = interaction.component.customId
+    if (customId.includes('ready')) {
+        setup.ready(interaction)
+    } else if (customId.includes('yes')) {
+        setup.yes(interaction)
+    } else if (customId.includes('no')) {
+        setup.no(interaction)
+    } else if (customId.includes('help')) {
+        setup.help(interaction)
+    }
+}
+
+/**
+ *
+ * @param {Interaction} interaction
+ */
+async function handleSetupSelectMenu(interaction) {
+    const util = new Util(interaction.member.id, interaction.guildId, interaction.client)
+    if (!util.isOwner()) {
+        return interaction.reply({
+            content: `Only the owner can change the settings!`,
+            ephemeral: true,
+        })
+    }
+    setup.prefix(interaction)
 }
