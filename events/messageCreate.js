d1e1bb56bc497e32ad70457c9994d4990464d1df
hoosengold <73896404+hoosengold@users.noreply.github.com>
2022-02-08 02:05:58 +0200

    Remove unnecessary try-catch blocks

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -26,92 +26,80 @@ module.exports = {
 
         utilObject.setPrefix(prefix)
 
+        //checks if the author of the message is a bot, if it is, then it does not respond
+        if (message.author.bot) return
+
+        if (await matchUrl(message, utilObject)) {
+            return message.delete()
+        }
+
+        //checks if the message is a possible command that follows the command syntax
+        const messageRegEx = new RegExp(/(\w)+\s*\*\s*(\w)*/gm)
+        if (!message.content.startsWith(prefix) || message.content.match(messageRegEx) || message.content == prefix) {
+            return
+        }
+        //takes the message body, removes the prefix *, splits the message body and makes everything lower case
+        const args = message.content.slice(prefix.length).split(/ +/), //returns args[] where [0] is the first word after the command
+            commandName = args.shift().toLowerCase() //returns the command
+
+        const command = (await client.commands.get(commandName)) || (await client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName)))
+
+        if (!command) return //check is the command exists
+
+        console.log(`command: ${commandName}`)
+        console.log(`args: ${args}`)
+
+        //cooldown for the specific command for the specific user
+        const { cooldowns } = client
+
+        //check if the cooldown collection already has a cooldown for the command
+        if (!cooldowns.has(command.name)) {
+            await cooldowns.set(command.name, new Collection())
+        }
+
+        const now = Date.now(),
+            timestamps = await cooldowns.get(command.name),
+            cooldownAmount = (command.cooldown || 5) * 1000
+
+        //get the timestamp and calculate the remaining time if the user already used the command in this session
+        if (timestamps.has(message.author.id)) {
+            const expirationDate = (await timestamps.get(message.author.id)) + cooldownAmount
+            if (now < expirationDate) {
+                //checks if there is still cooldown
+                const timeLeft = (expirationDate - now) / 1000
+                setTimeout(() => {
+                    message.delete().catch(console.error())
+                }, 1000)
+                return await message
+                    .reply({
+                        content: `Please wait ${timeLeft.toFixed(1)} seconds before using the ${command.name} command again.`,
+                        allowedMentions: { repliedUser: true },
+                    })
+                    .catch(console.error())
+            }
+        }
+        //clear the entry on the collection after the cooldown
+        await timestamps.set(message.author.id, now)
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
+        //execute the command
         try {
-            //checks if the author of the message is a bot, if it is, then it does not respond
-            if (message.author.bot) return
-
-            if (await matchUrl(message, utilObject)) {
-                return message.delete()
-            }
-
-            //checks if the message is a possible command that follows the command syntax
-            const messageRegEx = new RegExp(/(\w)+\s*\*\s*(\w)*/gm)
-            if (!message.content.startsWith(prefix) || message.content.match(messageRegEx) || message.content == '*') {
-                return
-            }
-
-            try {
-                //takes the message body, removes the prefix *, splits the message body and makes everything lower case
-                const args = message.content.slice(prefix.length).split(/ +/), //returns args[] where [0] is the first word after the command
-                    commandName = args.shift().toLowerCase() //returns the command
-
-                const command =
-                    (await client.commands.get(commandName)) || (await client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName)))
-
-                if (!command) return //check is the command exists
-
-                console.log(`command: ${commandName}`)
-                console.log(`args: ${args}`)
-
-                //cooldown for the specific command for the specific user
-                const { cooldowns } = client
-
-                //check if the cooldown collection already has a cooldown for the command
-                if (!cooldowns.has(command.name)) {
-                    await cooldowns.set(command.name, new Collection())
-                }
-
-                const now = Date.now(),
-                    timestamps = await cooldowns.get(command.name),
-                    cooldownAmount = (command.cooldown || 5) * 1000
-
-                //execute the command
-                try {
-                    //get the timestamp and calculate the remaining time if the user already used the command in this session
-                    if (timestamps.has(message.author.id)) {
-                        const expirationDate = (await timestamps.get(message.author.id)) + cooldownAmount
-                        if (now < expirationDate) {
-                            //checks if there is still cooldown
-                            const timeLeft = (expirationDate - now) / 1000
-                            setTimeout(() => {
-                                message.delete().catch(console.error())
-                            }, 1000)
-                            return await message
-                                .reply({
-                                    content: `Please wait ${timeLeft.toFixed(1)} seconds before using the ${command.name} command again.`,
-                                    allowedMentions: { repliedUser: true },
-                                })
-                                .catch(console.error())
-                        }
-                    }
-                    //clear the entry on the collection after the cooldown
-                    await timestamps.set(message.author.id, now)
-                    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
-                    //execute the command
-                    await command.execute(message, args, utilObject)
-                } catch (error) {
-                    console.log(`PROBLEM WHILE EXECUTING THE COMMAND`)
-                    console.error(error)
-                    setTimeout(() => {
-                        message.delete().catch(console.error())
-                    }, 1000)
-                    await message
-                        .reply({
-                            content: `Something went wrong while trying to execute the command!`,
-                            allowedMentions: { repliedUser: true },
-                        })
-                        .catch(console.error())
-                }
-            } catch (error) {
-                console.log(`PROBLEM WHILE SETTING UP THE COOLDOWN`)
-                console.error(error)
-            }
+            await command.execute(message, args, utilObject)
         } catch (error) {
-            console.log(`PROBLEM WHILE CHECKING URL`)
             console.error(error)
+            setTimeout(() => {
+                message.delete().catch(console.error())
+            }, 1000)
+            await message
+                .reply({
+                    content: `Something went wrong while trying to execute the command!`,
+                    allowedMentions: { repliedUser: true },
+                })
+                .catch(console.error())
         }
     },
 }
+
+//TODO move to ts object class
 
 /**
  * Checks if the message contains a link. If the link is a discord invite, that is not posted by a moderator, a shortened link or a possibly
